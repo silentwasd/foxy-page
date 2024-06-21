90a8d3623ab29debe2cd18a200ca774667f55e97
ilya.ignatev <ilya.ignatev@onlinesup.com>
2024-06-21 22:59:02 +0300

    Test component and public images

--- app.vue
+++ app.vue
@@ -1,6 +1,24 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+    <div class="overflow-hidden relative w-dvh h-dvh">
+        <Head>
+            <title>Главная</title>
+        </Head>
+
+        <div class="bg bg-fuchsia-300 w-dvw h-dvh fixed -z-10"></div>
+
+        <div class="overflow-auto w-full h-full">
+
+            <div class="container mx-auto max-w-4xl">
+                <div id="vhs">
+                    <div class="scan-lines"></div>
+                    <div class="vhs-wrap">
+                        <div class="vhs-meta">
+                        </div>
+                    </div>
+                </div>
+
+                <HelloBlock class="second-bg md:rounded-2xl p-10 md:my-10"/>
+            </div>
+        </div>
+    </div>
 </template>
---
+++ components/HelloBlock.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div>
+        <h1 class="text-4xl text-center font-semibold title">AssunaYuuki</h1>
+
+        <div class="mt-5 text-2xl text-block">
+            <img src="/img/fennec.png"
+                 class="h-full object-cover md:h-64 float-start mb-5 md:me-5 md:mb-2"/>
+
+            <p>
+                Привет! Я весёлая лисичка. Мне уже 900 лет, но в душе я молода и юна. А ещё я люблю общаться и
+                обниматься! Обнимемся?
+            </p>
+
+            <p class="mt-5">
+                Лисы умеют много чего, но каждая лиса не похожа на таких же других. Мы можем отличаться окрасом,
+                глазами, ушами и даже хвостом! Вот у меня длинный и пушистый. Я его очень люблю и ухаживаю за ним каждый
+                день. Правда-правда.
+            </p>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.title {
+    font-family: 'Amaranth', serif;
+}
+
+.text-block {
+    font-family: 'Caveat', sans-serif;
+}
+</style>
